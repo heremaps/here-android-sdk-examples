15be5a3a43f73f83eeac327b02aecc58c5deaaa9
Andrea Rossi <andrea.rossi@here.com>
2017-04-27 11:07:50 +0200

    Replace pushd and popd
    Since pushd and popd are not commands supported by sh, we can replace them
    with a basic cd.

--- .ci/buildAll.sh
+++ .ci/buildAll.sh
@@ -3,7 +3,7 @@
 # Find paths that contain the Gradle wrapper executable.
 APP_PROJECTS=$(find $PWD -maxdepth 1 -type d -exec [ -x {}/gradlew ] \; -print -prune)
 
-pushd . > /dev/null
+ORIG_PWD="$PWD"
 
 for APP_PATH in $APP_PROJECTS; do
     cd $APP_PATH
@@ -14,6 +14,6 @@ for APP_PATH in $APP_PROJECTS; do
     fi
 done
 
-popd > /dev/null
+cd "$ORIG_PWD"
 
 exit $ERROR
