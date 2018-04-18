13f9b4fb7cbeff6c6aef31f2530775bd84301c7d
starand <ext-andriy.starodub@here.com>
2018-04-18 10:06:16 +0300

    Updated copyright year

--- natural-language-processing/app/src/main/java/com/here/android/example/nlp/MyASR.java
+++ natural-language-processing/app/src/main/java/com/here/android/example/nlp/MyASR.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2017 HERE Global B.V. and its affiliate(s).
+ * Copyright (c) 2011-2018 HERE Global B.V. and its affiliate(s).
  * All rights reserved.
  * The use of this software is conditional upon having a separate agreement
  * with a HERE company for the use or utilization of this software. In the
