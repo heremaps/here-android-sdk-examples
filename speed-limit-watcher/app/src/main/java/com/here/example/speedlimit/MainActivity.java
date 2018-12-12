721aff445449157ac8be2cc808cc33c4d8aaf414
Nazar Kacharaba <ext-nazar.kacharaba@here.com>
2018-12-12 14:21:05 +0200

    Do not export MapService for other apps.
    Signed-off-by: Nazar Kacharaba <ext-nazar.kacharaba@here.com>

--- speed-limit-watcher/app/src/main/java/com/here/example/speedlimit/MainActivity.java
+++ speed-limit-watcher/app/src/main/java/com/here/example/speedlimit/MainActivity.java
@@ -2,13 +2,15 @@ package com.here.example.speedlimit;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.here.android.mpa.common.ApplicationContext;
@@ -17,6 +19,7 @@ import com.here.android.mpa.common.OnEngineInitListener;
 import com.here.android.mpa.common.PositioningManager;
 import com.here.android.mpa.guidance.NavigationManager;
 
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,8 +49,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initSDK() {
-        ApplicationContext appContext = new ApplicationContext(this);
+        // Set path of isolated disk cache
+        String diskCacheRoot = Environment.getExternalStorageDirectory().getPath()
+                + File.separator + ".isolated-here-maps";
+        // Retrieve intent name from manifest
+        String intentName = "";
+        try {
+            ApplicationInfo ai = getPackageManager().getApplicationInfo(getPackageName(),
+                    PackageManager.GET_META_DATA);
+            Bundle bundle = ai.metaData;
+            intentName = bundle.getString("INTENT_NAME");
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e("MainActivity",
+                    "Failed to find intent name, NameNotFound: " + e.getMessage());
+        }
+        boolean success = com.here.android.mpa.common.MapSettings.setIsolatedDiskCacheRootPath(
+                diskCacheRoot, intentName);
+        if (!success) {
+            Toast.makeText(this, "Operation 'setIsolatedDiskCacheRootPath' was not successful",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
 
+        ApplicationContext appContext = new ApplicationContext(this);
         MapEngine.getInstance().init(appContext, new OnEngineInitListener() {
             @Override
             public void onEngineInitializationCompleted(Error error) {
@@ -58,7 +82,8 @@ public class MainActivity extends AppCompatActivity {
                     activateSpeedLimitFragment();
 
                 } else {
-                    //handle error here
+                    Log.e("MainActivity", " init error: " + error + ", " + error.getDetails(),
+                            error.getThrowable());
                 }
             }
         });
