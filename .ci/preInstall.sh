fe33d8d731321ed6baba56b7372a07f584b4cb50
starand <starand@ukr.net>
2019-04-01 18:53:26 +0300

    Update preInstall.sh

--- .ci/preInstall.sh
+++ .ci/preInstall.sh
@@ -7,7 +7,7 @@ curl -s -S -o HERE_SDK.zip "$HERE_SDK_URL"
 unzip -j -o 'HERE_SDK.zip' -d 'HERE_SDK'
 
 # Extract the contained inner ZIP.
-unzip -j -o 'HERE_SDK/HERE-sdk.zip' 'HERE-sdk/HERE-sdk.aar' -d 'HERE_SDK'
+unzip -j -o 'HERE_SDK/HERE-sdk.zip' 'HERE-sdk/libs/HERE-sdk.aar' -d 'HERE_SDK'
 ls -al HERE_SDK/
 ls -al HERE_SDK/HERE_SDK/
 
