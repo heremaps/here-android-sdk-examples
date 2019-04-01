2c5fa128ba44321cde87c532f22c4622bf5b0160
starand <starand@ukr.net>
2019-04-01 18:12:18 +0300

    Update preInstall.sh

--- .ci/preInstall.sh
+++ .ci/preInstall.sh
@@ -7,7 +7,7 @@ curl -s -S -o HERE_SDK.zip "$HERE_SDK_URL"
 unzip -j -o 'HERE_SDK.zip' -d 'HERE_SDK'
 
 # Extract the contained inner ZIP.
-unzip -j -o 'HERE_SDK/HERE-sdk.zip' 'sdk/HERE-sdk/HERE-sdk.aar' -d 'HERE_SDK'
+unzip -j -o 'HERE_SDK/HERE-sdk.zip' 'HERE-sdk/libs/HERE-sdk.aar' -d 'HERE_SDK'
 
 # Find paths that contain an app module
 APP_PROJECTS=$(find "$PWD" -maxdepth 1 -type d -exec [ -d {}/app/libs ] \; -print -prune)
