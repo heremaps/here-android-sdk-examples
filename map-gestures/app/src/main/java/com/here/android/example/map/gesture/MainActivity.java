abf840f8b4db4d4b8cb43f7b5266e303ea43fe26
Nazar Kacharaba <ext-nazar.kacharaba@here.com>
2020-01-08 14:32:21 +0200

    Update copyright year to 2020.
    Signed-off-by: Nazar Kacharaba <ext-nazar.kacharaba@here.com>

--- map-gestures/app/src/main/java/com/here/android/example/map/gesture/MainActivity.java
+++ map-gestures/app/src/main/java/com/here/android/example/map/gesture/MainActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2019 HERE Europe B.V.
+ * Copyright (c) 2011-2020 HERE Europe B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
